8d7bcf30b701a9fbe4c764c95dbe71d44027a237
Francesco Montefoschi <francesco.monte@gmail.com>
2010-10-06 23:12:36 +0200

    created empty tree controller

---
+++ application/Controller/Tree.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * gittorama - a websvn for git
+ * Copyright (C) 2010  Francesco Montefoschi <francesco.monte@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/agpl-3.0.html  GNU AGPL 3.0
+ */
+
+class Controller_Tree extends Controller
+{
+	public function run()
+	{
+
+		$this->view->hash = $this->getParam('hash');
+
+		$this->render('tree');
+	}
+}
