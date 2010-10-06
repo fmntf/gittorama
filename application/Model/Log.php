0727457e33ca7e2fdf1b9d2270cdbf3e3d4cd47d
Francesco Montefoschi <francesco.monte@gmail.com>
2010-10-06 21:37:35 +0200

    first implementation of log parser

--- application/Model/Log.php
+++ application/Model/Log.php
@@ -25,20 +25,61 @@ class Model_Log
 
 	private $path;
 	private $hash;
+	private $info;
 
 	public function __construct($path, $hash)
 	{
 		$this->path = $path;
 		$this->hash = $hash;
+	}
 
-		$this->fetchInfo();
+	public function getInfo()
+	{
+		if ($this->info === null) {
+			$this->info = $this->fetchInfo();
+		}
+
+		return $this->info;
 	}
 
 	private function fetchInfo()
 	{
-		$result = shell_exec("git --git-dir={$this->path} rev-list {$this->hash} --max-count=1 --header");
+		$result = trim(shell_exec("git --git-dir={$this->path} rev-list {$this->hash} --max-count=1 --header"));
 
-//		var_dump($result);
+		$default = "[\*]?";
+		$noun = "[\w\-\.]+";
+		$hash = "[a-z0-9]+";
+		$return = "\n";
+		$space = "\s+";
+		$phrase = ".*";
+		$multiLinePharse = "[\w\W\s]*";
+		$contact = "<($noun@$noun\.$noun)>";
+		$time = "(\d+) \+(\d+)";
+
+		$pattern  = "/($hash)$return";
+		$pattern .= "tree ". "($hash)$return";
+		$pattern .= "author " . "($phrase)$space($contact)$space($time)$return";
+		$pattern .= "committer " . "($phrase)$space($contact)$space($time)$return";
+		$pattern .= "($multiLinePharse)/";
+
+		preg_match($pattern, $result, $matches);
+
+		return array(
+			'tree' => $matches[2],
+			'author' => array(
+				'name' => $matches[3],
+				'email' => $matches[5],
+				'timestamp' => $matches[7],
+				'offset' => $matches[8]
+			),
+			'committer' => array(
+				'name' => $matches[9],
+				'email' => $matches[11],
+				'timestamp' => $matches[13],
+				'offset' => $matches[14]
+			),
+			'message' => trim($matches[15])
+		);
 	}
 
 }
---
+++ testing/Model/LogTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * gittorama - a websvn for git
+ * Copyright (C) 2010  Francesco Montefoschi <francesco.monte@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/agpl-3.0.html  GNU AGPL 3.0
+ */
+
+class Model_LogTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testCanGetLogInfo()
+	{
+		$path = unpackRepository('simple');
+		$log = new Model_Log($path . '/.git', 'db57541ba4f7686c2063d7d1290eba612b30bf59');
+
+		$info = $log->getInfo();
+
+		$this->assertEquals('ac18b0b728d770f6864046e7d3b8c7f1ca09cf1f', $info['tree']);
+		$this->assertEquals('Simple commit message', $info['message']);
+		$this->assertEquals('Francesco Montefoschi', $info['author']['name']);
+		$this->assertEquals('francesco.monte@gmail.com', $info['author']['email']);
+		$this->assertEquals(1285956453, $info['author']['timestamp']);
+		$this->assertEquals($info['author'], $info['committer']);
+	}
+
+}
